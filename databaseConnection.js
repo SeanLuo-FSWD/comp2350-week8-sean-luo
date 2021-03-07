9b2b71c8f471a91a6bc418e16ee0523ad20fb01e
randomuser <luoxiao1106@gmail.com>
2021-03-07 09:47:41 -0800

    db connected

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,9 +4,9 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
 	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
+	user: "b7e39fcf1edef6",
+	password: "0e3a5140",
+	database: "heroku_4361c9a7ea9551c",
 	multipleStatements: false
 };
 
