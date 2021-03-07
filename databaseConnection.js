10aa7f1961fc73cc48bc68f310ccb330805200e5
randomuser <luoxiao1106@gmail.com>
2021-03-07 10:02:24 -0800

    switched db connection

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,14 +3,23 @@ const mysql = require('mysql2');
 const is_heroku = process.env.IS_HEROKU || false;
 
 
+// const dbConfigHeroku = {
+// 	host: "us-cdbr-east-03.cleardb.com",
+// 	user: "b7e39fcf1edef6",
+// 	password: "0e3a5140",
+// 	database: "heroku_4361c9a7ea9551c",
+// 	multipleStatements: false
+// };
+
 const dbConfigHeroku = {
 	host: "us-cdbr-east-03.cleardb.com",
-	user: "b7e39fcf1edef6",
-	password: "0e3a5140",
-	database: "heroku_4361c9a7ea9551c",
-	multipleStatements: false
-};
+	user: "b314d31c9e4e2d",
+	password: "800cb4bc",
+	database: "heroku_b44c5164b905863",
+	multipleStatements: false,
+  };
 
+  
 const dbConfigLocal = {
 	host: "localhost",
 	user: "root",
